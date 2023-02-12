bb9f1115b3e80e5761064c1ebbeb91ea839f6890
Blazes35 <loridam2@gmail.com>
2023-02-12 21:55:37 +0100

    pytest message do_test

--- do_test.py
+++ do_test.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     import pytest
     pytest.main(["."])
+
+# all the test aren't in a other folder, it's du to pytest which can't import a file in a parent file, even if python can do it   
