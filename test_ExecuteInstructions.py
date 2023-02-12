7c9082de23f6c302b6780b264179c90dc218a153
Yoween <larochetristan17@gmail.com>
2023-02-12 14:08:04 +0100

    progress on tests

--- test_ExecuteInstructions.py
+++ test_ExecuteInstructions.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+# from ExecuteInstructions import ExecuteInstructions
+# from main import UI_Window
+# import os
+# import tkinter as tk
+
+# def test_process():
+#     test1 = ExecuteInstructions()
+#     current_dir = os.getcwd()
+#     test_file = current_dir + "/test_program.ptm"
+#     assert test1.process(current_dir + "/default_templates/invert_values.ptm") == {'1': {'b': ('-', '<', '1'), '0': ('-', '-', '2'), '1': ('-', '-', '2')}, '2': {'b': ('-', '-', 'f'), '0': ('1', '<', '2'), '1': ('0', '<', '2')}}
